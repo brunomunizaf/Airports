7d4a0f7a0132b08579cd83c0c1feac3a244f567f
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2023-10-02 01:24:22 -0300

    Upload project

---
+++ Airports/AirportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportsApp.swift
+//  Airports
+//
+//  Created by Bruno Muniz on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AirportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
